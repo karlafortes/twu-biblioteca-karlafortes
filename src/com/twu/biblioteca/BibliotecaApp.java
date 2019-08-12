c5febea5ae10609b3f46ec2498e5c6f4acf74599
Karla Fortes <karlafortes@Karlas-MacBook-Pro.local>
2019-08-12 16:50:23 -0300

    [twu-biblioteca][release1][1.1] Adding a welcome message

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -2,7 +2,8 @@ package com.twu.biblioteca;
 
 public class BibliotecaApp {
 
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
     }
 }
