b608fbf591c724dc17b079ab20cc91b9021e32bd
Karla Fortes <karlafortes@Karlas-MacBook-Pro.local>
2019-08-13 11:23:29 -0300

    [twu-biblioteca][release1][1.4] Creating main menu of options

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -2,15 +2,36 @@ package com.twu.biblioteca;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class BibliotecaApp {
 
+    static Biblioteca biblioteca;
+
     public static void main(String[] args) {
 
-        Biblioteca biblioteca = new Biblioteca();
-        List<Book> books = biblioteca.getBooks();
+        biblioteca = new Biblioteca();
+
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
+        System.out.println();
+        System.out.println("Choose the number of the option would like to do:");
+        System.out.println();
+        System.out.println("1. List all books");
+        System.out.println();
+        int option = scanner.nextInt();
+
+        if(option == 1){
+
+            ListBook();
+        }
+    }
+
+    public static void ListBook(){
+
+        List<Book> books = biblioteca.getBooks();
+
         System.out.println();
         System.out.println("This list contains all of our awesome books. Check it out:");
         System.out.println();
