7d4ee1e363b7e4a51cbaa96b56e341dd2bbf2cb5
Karla Fortes <karlafortes@Karlas-MacBook-Pro.local>
2019-08-13 20:54:16 -0300

    [twu-biblioteca][release1][1.6] Implemeting the option to quit the application

---
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -0,0 +1,19 @@
+package com.twu.biblioteca;
+
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertThat;
+
+public class BibliotecaAppTest {
+
+    @Test
+    public void ListBook() {
+
+        Biblioteca biblioteca = new Biblioteca();
+
+       // assertThat(1, 1);
+    }
+}
