e6a9d1668543c3cb585a9816b33174b3db33232f
Karla Fortes <karlafortes@Karlas-MacBook-Pro.local>
2019-08-13 10:21:51 -0300

    [twu-biblioteca][release1][1.3] Listing authors and years od the books

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -5,30 +5,29 @@ import java.util.List;
 
 public class BibliotecaApp {
 
-    static List<String> Books = new ArrayList<String>()
-    {
+    static List<Book> Books = new ArrayList<Book>() {
         {
-            add("Artificial Intelligence: A Modern Approach");
-            add("Clean Code");
-            add("Dreamcatcher");
-            add("It");
-            add("Paper towns");
-            add("Sapiens: A Brief History of Humankind");
-            add("Test Driven Development: By Example");
-            add("The fault in our stars");
-            add("The shinning" );
+            add(new Book("Artificial Intelligence: A Modern Approach", "Peter Norvig and Stuart J. Russell", "1994"));
+            add(new Book("Clean Code", "",""));
+            add(new Book("Dreamcatcher","",""));
+            add(new Book("It","", ""));
+            add(new Book("Paper towns", "", ""));
+            add(new Book("Sapiens: A Brief History of Humankind", "", ""));
+            add(new Book("Test Driven Development: By Example", "", ""));
+            add(new Book("The fault in our stars", "", ""));
+            add(new Book("The shinning", "", ""));
         }
     };
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
+
         System.out.println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
         System.out.println();
         System.out.println("This list contains all of our awesome books. Check it out:");
         System.out.println();
 
-        for (String book:Books) {
-            System.out.println("- " + book);
+        for (Book book:Books) {
+            System.out.println("- " + book.getTitle());
         }
     }
 }
